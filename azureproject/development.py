86ad3fc850652cd31966b9d284885b842ef4f188
vmagelo <v-gelorminom@microsoft.com>
2022-10-11 09:07:45 +0200

    Updates to use new version of SQL Alchemy.

--- azureproject/development.py
+++ azureproject/development.py
@@ -6,18 +6,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-# Local to instance settings.
-DBHOST=os.environ['LOCAL_HOST']
-DBNAME=os.environ['LOCAL_DATABASE']
-DBUSER=os.environ['LOCAL_USERNAME']
-DBPASS=os.environ['LOCAL_PASSWORD']
-
 # Configure database connection for remote PostgreSQL instance.
 if 'USE_REMOTE_POSTGRESQL' in os.environ:
-    DBNAME=os.environ['AZURE_POSTGRESQL_DATABASE']
     DBHOST=os.environ['AZURE_POSTGRESQL_HOST']
+    DBNAME=os.environ['AZURE_POSTGRESQL_DATABASE']
     DBUSER=os.environ['AZURE_POSTGRESQL_USERNAME']
     DBPASS=os.environ['AZURE_POSTGRESQL_PASSWORD']
+else:
+    # Local to instance settings.
+    DBHOST=os.environ['LOCAL_HOST']
+    DBNAME=os.environ['LOCAL_DATABASE']
+    DBUSER=os.environ['LOCAL_USERNAME']
+    DBPASS=os.environ['LOCAL_PASSWORD']
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=DBUSER,
